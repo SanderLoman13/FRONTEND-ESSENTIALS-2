6ed66ea253044f2c456b1c0874b54c66b9ddbd61
Sander Loman <sander.loman@student.rocva.nl>
2021-02-05 10:47:48 +0100

    taak 2-1 af

--- 02-Javascript/taak01-Herhaling/js/script.js
+++ 02-Javascript/taak01-Herhaling/js/script.js
@@ -1,2 +1,4 @@
-let strNaam;
-let boolNaamKlopt;
+let strNaam = prompt("Wat is je naam?");
+let boolNaamKlopt = prompt("Nog één keer, hoe heet je?");
+alert("heet je" + " " + boolNaamKlopt + "?");
+alert("Ok, evengoed welkom!")
