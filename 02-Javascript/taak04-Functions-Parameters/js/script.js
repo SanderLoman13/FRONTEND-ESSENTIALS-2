c5a735d1e89b8fb360ea5296eb717c5740c1a962
Sander Loman <sander.loman@student.rocva.nl>
2021-02-10 12:17:53 +0100

    taak 2-4 af

---
+++ 02-Javascript/taak04-Functions-Parameters/js/script.js
@@ -0,0 +1,9 @@
+let getal1 = (prompt("Voer hier het 1ste getal in."));
+let getal2 = (prompt("Voer hier het 2de getal in."));
+
+function telOp(){
+    let getal3 = getal1 * getal2
+    return getal3
+}
+console.log(telOp());
+
