8e74b7777e66fc5cd73f3f98de685d7d1a629347
Sander Loman <sander.loman@student.rocva.nl>
2021-02-19 12:06:25 +0100

    snap deze opdracht niet

--- 02-Javascript/taak06-Functions-Samenvatting/js/script.js
+++ 02-Javascript/taak06-Functions-Samenvatting/js/script.js
@@ -1,5 +1,6 @@
-var score = 0;
+let score = 0;
 let getal1;
+let getal2;
 
 function makeRandomNumber(){
     return Math.floor(Math.random()*99+1);
@@ -10,5 +11,30 @@ function showRandomNumber1(){
     document.querySelector(".getal1").innerHTML = randomGetal;
     return randomGetal;
 }
+function showRandomNumber2(){
+    let randomGetal = makeRandomNumber()
+    document.querySelector(".getal2").innerHTML = randomGetal;
+    return randomGetal;
+}
 
-getal1 = showRandomNumber1();
+function geefAntwoord(antwoord){
+    let score = 0;
+    getal1 = document.querySelector(".getal1").innerHTML;
+    getal2 = document.querySelector(".getal2").innerHTML;
+    score = document.querySelector(".score").innerHTML;
+    alert("x" + score + "x");
+    if (getal1 > getal2 && antwoord == 'hoger'){
+       alert("goed");
+       parseInt(score = score + 1);
+       document.querySelector(".score").innerHTML = score;
+    }
+    if (getal1 < getal2 && antwoord == 'lager'){
+        alert("ook goed");
+        parseInt(score = score + 1);
+        document.querySelector(".score").innerHTML = score;
+    }
+    showRandomNumber1();
+    showRandomNumber2();
+}
+showRandomNumber1();
+showRandomNumber2();
