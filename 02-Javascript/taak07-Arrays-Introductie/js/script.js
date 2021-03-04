892f17d0b5525c1871a47b3b30639336f6f07c36
Sander Loman <sander.loman@student.rocva.nl>
2021-03-04 13:42:33 +0100

    taak 2-7 af

---
+++ 02-Javascript/taak07-Arrays-Introductie/js/script.js
@@ -0,0 +1,4 @@
+topGames = ['Minecraft','GTA 5','Assasins Greed']
+topSeriesOfFilms = ['The Punisher','Advengers Endgame','Rick and Morty']
+topMuziek = ['Falling down - XXXtentacion, Lil Peep','Lucid Dreams - Juice WRLD','Wishing Well - Juice WRLD']
+console.log(topGames, topSeriesOfFilms, topMuziek)
