4172af911bdff382f4cb0f3fd689a206971e3492
Sander Loman <sander.loman@student.rocva.nl>
2021-03-12 12:18:27 +0100

    taak 2-9 af

--- 02-Javascript/taak09-Arrays-Toevoegen/js/script.js
+++ 02-Javascript/taak09-Arrays-Toevoegen/js/script.js
@@ -18,6 +18,24 @@ for(i = 0; i < topFilms.length; i++){
     document.querySelector(".imdb-film" + (i+1)).innerHTML = topFilms[i];
 }
 
+var filmnummer = 1; 
+ 
 function kiesFilm(){
+    nummer = prompt("Welke Film staat op plek " + filmnummer);
+        if(nummer >= 0 && nummer <= 10 ){
+            eigenTopFilms.push(topFilms[nummer]); 
+            naam = document.querySelector(".imdb-film" + nummer).innerHTML;
+            let toegevoegd1 = document.querySelector(".eigen-film" + filmnummer);
+            toegevoegd1.innerHTML = naam;
+            console.log(naam);
+            document.querySelector(".imdb-film" + nummer).style.textDecoration = "line-through";;
+            filmnummer++;
 
+            if(filmnummer >= 4){
+                document.querySelector('.kiesfilm-button').innerText = 'Klaar';
+            }
+            else{
+                document.querySelector('.kiesfilm-button').innerText = 'Kies Film voor plek ' + filmnummer;
+            }
+        }
 }
